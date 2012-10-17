54f936cb269084cb4c2a7b233d47f5a5c19432ca
Masahiro Hayashi <mhayashi1120@gmail.com>
2012-10-17 23:50:43 +0900

    add new test for lazy parser

--- pcsv-test.el
+++ pcsv-test.el
@@ -28,3 +28,39 @@
   :tags '(pcsv)
   (should (pcsv-test-get (make-string (ash 1 10) ?a)))
   (should (pcsv-test-get (make-string (ash 1 20) ?a))))
+
+(ert-deftest pcsv-lazy-parser-test1 ()
+  "Check handling lazy parser."
+  :tags '(pcsv)
+  (with-temp-buffer
+    (insert "a,b,c\n")
+    (insert "A,B,C\n")
+    (let ((parser (pcsv-parser)))
+      (should (equal (funcall parser) '("a" "b" "c")))
+      (should (equal (funcall parser) '("A" "B" "C")))
+      (should-not (funcall parser)))))
+
+(ert-deftest pcsv-lazy-parser-test2 ()
+  "Check handling lazy parser."
+  :tags '(pcsv)
+  (with-temp-buffer
+    (insert "a,\"b\",c")
+    (let ((parser (pcsv-parser)))
+      (should (equal (funcall parser) '("a" "b" "c")))
+      (should-not (funcall parser)))))
+
+(ert-deftest pcsv-lazy-file-parser-test ()
+  "Check handling lazy file parser."
+  :tags '(pcsv)
+  (let ((file (make-temp-file "pcsv-")))
+    (let ((coding-system-for-write 'euc-jp))
+      (write-region "あ,い,う\nA,B,C\n" nil file))
+    (let ((parser (pcsv-file-parser file 'euc-jp 1)))
+      (should (equal (funcall parser) '("あ" "い" "う")))
+      (should (equal (funcall parser) '("A" "B" "C")))
+      (should-not (funcall parser)))
+    (let ((parser (pcsv-file-parser file 'euc-jp 1)))
+      (should (equal (funcall parser) '("あ" "い" "う")))
+      (funcall parser t)
+      ;; raise no error
+      (should-not (funcall parser)))))
