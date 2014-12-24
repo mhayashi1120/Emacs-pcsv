cc19a5c54f48c723f9a94a9e59f111135b46d38e
Masahiro Hayashi <mhayashi1120@gmail.com>
2014-12-25 00:51:16 +0900

    update doc

--- pcsv.el
+++ pcsv.el
@@ -27,21 +27,21 @@
 ;; pcsv provides parser of csv based on rfc4180
 ;; http://www.rfc-editor.org/rfc/rfc4180.txt
 
-;;; Install:
+;; ## Install:
 
 ;; Put this file into load-path'ed directory, and byte compile it if
 ;; desired. And put the following expression into your ~/.emacs.
 ;;
 ;;     (require 'pcsv)
 
-;;; Usage:
+;; ## Usage:
 
-;; Use `pcsv-parse-buffer', `pcsv-parse-file', `pcsv-parse-region' functions
+;; Use `pcsv-parse-buffer`, `pcsv-parse-file`, `pcsv-parse-region` functions
 ;; to parse csv.
 
-;; To handle huge csv file, use the lazy parser `pcsv-file-parser'.
+;; To handle huge csv file, use the lazy parser `pcsv-file-parser`.
 
-;; To handle csv buffer like cursor, use the `pcsv-parser'.
+;; To handle csv buffer like cursor, use the `pcsv-parser`.
 
 ;;; Code:
 
